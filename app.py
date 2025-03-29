d800494d243c1a156b16855f7d2a56251c6a0d2c
heouol <youshipertube@gmail.com>
2025-03-29 13:54:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime, timedelta
 import json
 import os
-import scrims
 from scrims import scrims_page# Импорт функции scrims_page из файла scrims.py
 
 # Set page config at the start (must be the first Streamlit command)
